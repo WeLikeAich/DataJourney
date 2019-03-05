d564f3b8e2fdc932869c5bfb7a9713e07beb7fce
Mfeany <milkandoreos@yahoo.com>
2019-03-04 19:52:00 -0800

    Created polymorphism lab

--- src/main/java/lab/Polymorphism.java
+++ src/main/java/lab/Polymorphism.java
@@ -2,26 +2,25 @@ package main.java.lab;
 
 import main.java.models.Knight;
 import main.java.models.Monk;
-import main.java.models.Party;
 import main.java.models.Priest;
 import main.java.models.Rogue;
+import main.java.models.Character;
 
 public class Polymorphism {
 
 	public static void main(String[] args) {
 
-		Party PlayerParty = new Party(new Knight(), new Monk(), new Priest(), new Rogue());
+		Character[] Party = new Character[] { new Knight(), new Monk(), new Priest(), new Rogue() };
 
 		Knight TargetDummy = new Knight("TargetDummy");
 
-		for (int i = 0; i < PlayerParty.Members.length; i++) {
+		for (int i = 0; i < Party.length; i++) {
 
-			PlayerParty.Members[i].attack(TargetDummy);
-			System.out.printf("%s attacks %s %n", PlayerParty.Members[i].getName(), TargetDummy.getName());
+			Party[i].attack(TargetDummy);
+			System.out.printf("%s attacks %s %n", Party[i].getName(), TargetDummy.getName());
 
-			PlayerParty.Members[i].useSpecial(TargetDummy);
-			System.out.printf("%s uses %s on %s", PlayerParty.Members[i].getName(), PlayerParty.Members[i].SpecialName,
-					TargetDummy.getName());
+			Party[i].useSpecial(TargetDummy);
+			System.out.printf("%s uses %s on %s", Party[i].getName(), Party[i].SpecialName, TargetDummy.getName());
 
 		}
 
